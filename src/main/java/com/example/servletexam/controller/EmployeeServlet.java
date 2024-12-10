edc2dc6ba6fc13a0cd33d067daf4e8228daaafcf
Khuc Nhan <khucvii@gmail.com>
2024-12-10 14:13:19 +0700

    feature show pages

--- src/main/java/com/example/servletexam/controller/EmployeeServlet.java
+++ src/main/java/com/example/servletexam/controller/EmployeeServlet.java
@@ -1,4 +1,104 @@
 package com.example.servletexam.controller;
 
-public class EmployeeServlet {
+import com.example.servletexam.model.Employee;
+import com.example.servletexam.service.EmployeeService;
+import com.example.servletexam.service.EmployeeServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class EmployeeServlet extends HttpServlet {
+    private final EmployeeService employeeService = new EmployeeServiceImpl();
+
+    private void showCreateForm(HttpServletRequest req, HttpServletResponse resp) {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("employee/create.jsp");
+
+        try {
+            dispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showEditForm(HttpServletRequest req, HttpServletResponse resp) {
+        int id = Integer.parseInt(req.getParameter("id"));
+        Employee employee = this.employeeService.findById(id);
+        RequestDispatcher dispatcher;
+
+        if (employee == null) {
+            dispatcher = req.getRequestDispatcher("error-404.jsp");
+        } else {
+            req.setAttribute("employee", employee);
+            dispatcher = req.getRequestDispatcher("employee/delete.jsp");
+        }
+
+        try {
+            dispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showDeleteForm(HttpServletRequest req, HttpServletResponse resp) {
+        int id = Integer.parseInt(req.getParameter("id"));
+        Employee employee = this.employeeService.findById(id);
+        RequestDispatcher dispatcher;
+
+        if (employee == null) {
+            dispatcher = req.getRequestDispatcher("error-404.jsp");
+        } else {
+            req.setAttribute("employee", employee);
+            dispatcher = req.getRequestDispatcher("employee/delete.jsp");
+        }
+
+        try {
+            dispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void viewEmployee(HttpServletRequest req, HttpServletResponse resp) {
+        int id = Integer.parseInt(req.getParameter("id"));
+        Employee employee = this.employeeService.findById(id);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("employee/view.jsp");
+
+        if (employee == null) {
+            dispatcher = req.getRequestDispatcher("error-404.jsp");
+        } else {
+            req.setAttribute("employee", employee);
+            dispatcher = req.getRequestDispatcher("employee/delete.jsp");
+        }
+
+        try {
+            req.setAttribute("employee", employee);
+            dispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showEmployees(HttpServletRequest req, HttpServletResponse resp) {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("employee/list.jsp");
+        List<Employee> employees = this.employeeService.findAllEmployee();
+
+        if (employees == null) {
+            dispatcher = req.getRequestDispatcher("error-404.jsp");
+        } else {
+            req.setAttribute("employees", employeeService.findAllEmployee());
+            dispatcher = req.getRequestDispatcher("employee/delete.jsp");
+        }
+
+        try {
+            dispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
